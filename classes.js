d6ad8c2345346ba0ad14bfec71645ebe32ce3e92
researchersec <109282836+researchersec@users.noreply.github.com>
2026-05-08 03:20:21 +0200

    Add classColors object for character classes

---
+++ classes.js
@@ -0,0 +1,13 @@
+const classColors = {
+    1: 'warrior',
+    2: 'paladin',
+    3: 'hunter',
+    4: 'rogue',
+    5: 'priest',
+    6: 'deathknight',
+    7: 'shaman',
+    8: 'mage',
+    9: 'warlock',
+    10: 'monk',
+    11: 'druid'
+};
