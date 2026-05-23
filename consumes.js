0e0079f4115b0801032cde57c0f89bb2db6b1676
researchersec <109282836+researchersec@users.noreply.github.com>
2026-05-23 14:13:10 +0200

    Refactor consumables structure to include icons

--- consumes.js
+++ consumes.js
@@ -1,98 +1,304 @@
 {
   "consumables": {
     "flasks": {
-      "28507": "Flask of Relentless Assault",
-      "28508": "Flask of Pure Death",
-      "28521": "Flask of Blinding Light",
-      "42735": "Flask of Chromatic Wonder",
-      "28518": "Flask of Fortification",
-      "28519": "Flask of Mighty Restoration",
-      "17628": "Flask of Supreme Power",
-      "17627": "Flask of Distilled Wisdom",
-      "17626": "Flask of the Titans",
-      "17629": "Flask of Chromatic Resistance",
-      "41608": "Shattrath Flask of Relentless Assault",
-      "41609": "Shattrath Flask of Pure Death",
-      "41610": "Shattrath Flask of Blinding Light",
-      "41611": "Shattrath Flask of Fortification",
-      "41612": "Shattrath Flask of Mighty Restoration",
-      "41613": "Shattrath Flask of Supreme Power",
-      "40567": "Unstable Flask of the Beast",
-      "40568": "Unstable Flask of the Sorcerer",
-      "40572": "Unstable Flask of the Bandit",
-      "40573": "Unstable Flask of the Elder",
-      "40575": "Unstable Flask of the Physician",
-      "40576": "Unstable Flask of the Soldier"
+      "28507": {
+        "name": "Flask of Relentless Assault",
+        "icon": "inv_potion_158"
+      },
+      "28508": {
+        "name": "Flask of Pure Death",
+        "icon": "inv_potion_155"
+      },
+      "28521": {
+        "name": "Flask of Blinding Light",
+        "icon": "inv_potion_33"
+      },
+      "42735": {
+        "name": "Flask of Chromatic Wonder",
+        "icon": "inv_potion_152"
+      },
+      "28518": {
+        "name": "Flask of Fortification",
+        "icon": "inv_potion_156"
+      },
+      "28519": {
+        "name": "Flask of Mighty Restoration",
+        "icon": "inv_potion_157"
+      },
+      "17628": {
+        "name": "Flask of Supreme Power",
+        "icon": "inv_potion_41"
+      },
+      "17627": {
+        "name": "Flask of Distilled Wisdom",
+        "icon": "inv_potion_42"
+      },
+      "17626": {
+        "name": "Flask of the Titans",
+        "icon": "inv_potion_43"
+      },
+      "17629": {
+        "name": "Flask of Chromatic Resistance",
+        "icon": "inv_potion_44"
+      },
+      "41608": {
+        "name": "Shattrath Flask of Relentless Assault",
+        "icon": "inv_potion_158"
+      },
+      "41609": {
+        "name": "Shattrath Flask of Pure Death",
+        "icon": "inv_potion_155"
+      },
+      "41610": {
+        "name": "Shattrath Flask of Blinding Light",
+        "icon": "inv_potion_33"
+      },
+      "41611": {
+        "name": "Shattrath Flask of Fortification",
+        "icon": "inv_potion_156"
+      },
+      "41612": {
+        "name": "Shattrath Flask of Mighty Restoration",
+        "icon": "inv_potion_157"
+      },
+      "41613": {
+        "name": "Shattrath Flask of Supreme Power",
+        "icon": "inv_potion_41"
+      },
+      "40567": {
+        "name": "Unstable Flask of the Beast",
+        "icon": "inv_potion_159"
+      },
+      "40568": {
+        "name": "Unstable Flask of the Sorcerer",
+        "icon": "inv_potion_160"
+      },
+      "40572": {
+        "name": "Unstable Flask of the Bandit",
+        "icon": "inv_potion_161"
+      },
+      "40573": {
+        "name": "Unstable Flask of the Elder",
+        "icon": "inv_potion_162"
+      },
+      "40575": {
+        "name": "Unstable Flask of the Physician",
+        "icon": "inv_potion_163"
+      },
+      "40576": {
+        "name": "Unstable Flask of the Soldier",
+        "icon": "inv_potion_164"
+      }
     },
 
     "food": {
-      "33286": "Blackened Basilisk",
-      "33263": "Crunchy Serpent",
-      "33256": "Roasted Clefthoof",
-      "33261": "Warp Burger",
-      "33293": "Grilled Mudfish",
-      "42302": "Fisherman's Feast",
-      "33296": "Spicy Crawdad",
-      "33295": "Golden Fish Sticks",
-      "43730": "Skullfish Soup",
-      "43764": "Spicy Hot Talbuk",
-      "33289": "Talbuk Steak",
-      "33292": "Blackened Sporefish",
-      "38867": "Mok'Nathal Shortribs",
-      "43772": "Kibler's Bits",
-      "33284": "Ravager Dog",
-      "42304": "Hot Buttered Trout",
-      "42305": "Stewed Trout",
-      "43761": "Broiled Bloodfin"
+      "33286": {
+        "name": "Blackened Basilisk",
+        "icon": "inv_misc_food_45"
+      },
+      "33263": {
+        "name": "Crunchy Serpent",
+        "icon": "inv_misc_food_47"
+      },
+      "33256": {
+        "name": "Roasted Clefthoof",
+        "icon": "inv_misc_food_67"
+      },
+      "33261": {
+        "name": "Warp Burger",
+        "icon": "inv_misc_food_65"
+      },
+      "33293": {
+        "name": "Grilled Mudfish",
+        "icon": "inv_misc_fish_22"
+      },
+      "42302": {
+        "name": "Fisherman's Feast",
+        "icon": "inv_misc_food_76"
+      },
+      "33296": {
+        "name": "Spicy Crawdad",
+        "icon": "inv_misc_food_51"
+      },
+      "33295": {
+        "name": "Golden Fish Sticks",
+        "icon": "inv_misc_food_48"
+      },
+      "43730": {
+        "name": "Skullfish Soup",
+        "icon": "inv_misc_food_50"
+      },
+      "43764": {
+        "name": "Spicy Hot Talbuk",
+        "icon": "inv_misc_food_70"
+      },
+      "33289": {
+        "name": "Talbuk Steak",
+        "icon": "inv_misc_food_69"
+      },
+      "33292": {
+        "name": "Blackened Sporefish",
+        "icon": ""
+      },
+      "38867": {
+        "name": "Mok'Nathal Shortribs",
+        "icon": "inv_misc_food_68"
+      },
+      "43772": {
+        "name": "Kibler's Bits",
+        "icon": "inv_misc_food_71"
+      },
+      "33284": {
+        "name": "Ravager Dog",
+        "icon": "inv_misc_food_66"
+      },
+      "42304": {
+        "name": "Hot Buttered Trout",
+        "icon": "inv_misc_food_52"
+      },
+      "42305": {
+        "name": "Stewed Trout",
+        "icon": "inv_misc_food_53"
+      },
+      "43761": {
+        "name": "Broiled Bloodfin",
+        "icon": "inv_misc_food_49"
+      }
     },
 
     "potions": {
-      "38911": "Super Mana Potion",
-      "38908": "Super Healing Potion",
-      "28508": "Destruction Potion",
-      "28507": "Haste Potion",
-      "28579": "Ironshield Potion",
-      "28506": "Heroic Potion",
-      "28504": "Major Dreamless Sleep Potion"
+      "38911": {
+        "name": "Super Mana Potion",
+        "icon": "inv_potion_76"
+      },
+      "38908": {
+        "name": "Super Healing Potion",
+        "icon": "inv_potion_54"
+      },
+      "28508": {
+        "name": "Destruction Potion",
+        "icon": "inv_potion_21"
+      },
+      "28507": {
+        "name": "Haste Potion",
+        "icon": "inv_potion_95"
+      },
+      "28579": {
+        "name": "Ironshield Potion",
+        "icon": "inv_potion_69"
+      },
+      "28506": {
+        "name": "Heroic Potion",
+        "icon": "inv_potion_96"
+      },
+      "28504": {
+        "name": "Major Dreamless Sleep Potion",
+        "icon": "inv_potion_83"
+      }
     },
 
     "battle_elixirs": {
-      "28497": "Elixir of Major Agility",
-      "28490": "Elixir of Major Strength",
-      "33740": "Adept's Elixir",
-      "28503": "Elixir of Major Shadow Power",
-      "28501": "Elixir of Major Firepower",
-      "33720": "Onslaught Elixir",
-      "11406": "Elixir of Demonslaying",
-      "17538": "Elixir of the Mongoose",
-      "28491": "Elixir of Healing Power"
+      "28497": {
+        "name": "Elixir of Major Agility",
+        "icon": "inv_potion_94"
+      },
+      "28490": {
+        "name": "Elixir of Major Strength",
+        "icon": "inv_potion_93"
+      },
+      "33740": {
+        "name": "Adept's Elixir",
+        "icon": "inv_potion_90"
+      },
+      "28503": {
+        "name": "Elixir of Major Shadow Power",
+        "icon": "inv_potion_81"
+      },
+      "28501": {
+        "name": "Elixir of Major Firepower",
+        "icon": "inv_potion_80"
+      },
+      "33720": {
+        "name": "Onslaught Elixir",
+        "icon": "inv_potion_89"
+      },
+      "11406": {
+        "name": "Elixir of Demonslaying",
+        "icon": "inv_potion_79"
+      },
+      "17538": {
+        "name": "Elixir of the Mongoose",
+        "icon": "inv_potion_78"
+      },
+      "28491": {
+        "name": "Elixir of Healing Power",
+        "icon": "inv_potion_85"
+      }
     },
 
     "guardian_elixirs": {
-      "39625": "Elixir of Major Fortitude",
-      "28509": "Elixir of Major Mageblood",
-      "39627": "Elixir of Draenic Wisdom"
+      "39625": {
+        "name": "Elixir of Major Fortitude",
+        "icon": "inv_potion_86"
+      },
+      "28509": {
+        "name": "Elixir of Major Mageblood",
+        "icon": "inv_potion_82"
+      },
+      "39627": {
+        "name": "Elixir of Draenic Wisdom",
+        "icon": "inv_potion_87"
+      }
     },
 
     "scrolls": {
-      "33077": "Scroll of Agility V",
-      "33079": "Scroll of Protection V",
-      "33082": "Scroll of Strength V",
-      "28714": "Flame Cap"
-      
-
+      "33077": {
+        "name": "Scroll of Agility V",
+        "icon": ""
+      },
+      "33079": {
+        "name": "Scroll of Protection V",
+        "icon": ""
+      },
+      "33082": {
+        "name": "Scroll of Strength V",
+        "icon": ""
+      },
+      "28714": {
+        "name": "Flame Cap",
+        "icon": ""
+      }
     },
 
     "weapon_oils": {
-      "2677": "Superior Mana Oil",
-      "2678": "Superior Wizard Oil",
-      "2628": "Brilliant Wizard Oil",
-      
-      "2685": "Adamantite Weightstone2",
-      "2713": "Adamantite Sharpening Stone",
-
-      "2641": "Instant Poison VII",
-      "2643": "Deadly Poison VII"
+      "2677": {
+        "name": "Superior Mana Oil",
+        "icon": "inv_potion_101"
+      },
+      "2678": {
+        "name": "Superior Wizard Oil",
+        "icon": "inv_potion_100"
+      },
+      "2628": {
+        "name": "Brilliant Wizard Oil",
+        "icon": "inv_potion_99"
+      },
+      "2685": {
+        "name": "Adamantite Weightstone2",
+        "icon": ""
+      },
+      "2713": {
+        "name": "Adamantite Sharpening Stone",
+        "icon": ""
+      },
+      "2641": {
+        "name": "Instant Poison VII",
+        "icon": ""
+      },
+      "2643": {
+        "name": "Deadly Poison VII",
+        "icon": ""
+      }
     }
   }
 }
